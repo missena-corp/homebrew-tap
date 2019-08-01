58ecfcf9bcdac8cc8fa736737cfc0286d953e316
Gregory Guillou <gregory.guillou@resetlogs.com>
2019-08-01 20:44:46 +0200

    Upgrade to 0.0.32 (#5)

--- Formula/gally.rb
+++ Formula/gally.rb
@@ -1,8 +1,8 @@
 class Gally < Formula
   desc "A tool to manage projects in a mono-repository"
   homepage "https://github.com/missena-corp/gally"
-  url "https://github.com/missena-corp/gally/releases/download/v0.0.31/gally_0.0.31_darwin_x86_64.tar.gz"
-  sha256 "097d4083fb5a0ff73fa158148330f8dda8fedf235e0b1e8492ca6d6772d5b144"
+  url "https://github.com/missena-corp/gally/releases/download/v0.0.32/gally_0.0.32_darwin_x86_64.tar.gz"
+  sha256 "1afd96c6fb05d0400f64a0a77f1a077958cb37dbd36c485d450303faa0b1b568"
   
   depends_on "git"
  
